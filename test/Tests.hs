e4fd49e3dc5cb5567864c0e399b4294522083a61
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-05-29 10:33:06 +0200

    [ test ] expect build-tool goldplate on the PATH

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,10 +1,5 @@
 import System.Exit     ( exitWith  )
 import System.Process  ( system    )
-import System.FilePath ( (</>)     )
-
-import Paths_agda2lagda ( getBinDir )
 
 main :: IO ()
-main = do
-  bindir <- getBinDir
-  exitWith =<< system (bindir </> "goldplate test")
+main = exitWith =<< system ("goldplate test")
